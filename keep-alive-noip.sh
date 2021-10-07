9fc99ee814d9d5eeb596bc6bceba52c81514b9d5
ms-afk <>
2021-10-07 19:39:01 +0200

    Added logging features

--- keep-alive-noip.sh
+++ keep-alive-noip.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-noip2 > /dev/null 2>&1
+if test -f "/usr/local/etc/no-ip2.conf"; then
+	echo "[*]Container created, use noip2 -C to log in your account"
+	noip2 > /dev/null 2>&1
+else
+	echo "[*]Starting the noip2 daemon"
+	noip2
+fi
 
 while true
 do
